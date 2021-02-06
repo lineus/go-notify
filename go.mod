92e8df0966c6970a03a7028741a2dda7e5fe4b65
Kevin Huff <lineus@gmail.com>
2021-02-06 06:12:15 -0500

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/lineus/go-notify
+
+go 1.15
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.1.0
+	github.com/aws/aws-sdk-go-v2/config v1.1.0
+	github.com/aws/aws-sdk-go-v2/service/sns v1.1.0
+)
---
+++ notify.go
@@ -0,0 +1,39 @@
+package notify
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+)
+
+// SNSPublishAPI words
+type SNSPublishAPI interface {
+	Publish(ctx context.Context,
+		params *sns.PublishInput,
+		optFns ...func(*sns.Options)) (*sns.PublishOutput, error)
+}
+
+// Notify notifies via sms via aws
+func Notify(msg string) error {
+	config, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.PrintF("%+v\n", config)
+
+	client := sns.NewFromConfig(config)
+
+	input := &sns.PublishInput{
+		Message:  aws.String(msg),
+		TopicArn: aws.String(arn),
+	}
+
+	// _, err = client.Publish(context.TODO(), input)
+
+	return err
+}
